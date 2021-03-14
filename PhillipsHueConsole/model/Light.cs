eb92ee5a3cc480b0b72b8a04ffd9835926dd9f25
Jeppe <jeppe.bach248@hotmail.com>
2021-03-14 16:34:14 +0100

    encapsulated lightstate fully in seperate class

--- PhillipsHueConsole/model/Light.cs
+++ PhillipsHueConsole/model/Light.cs
@@ -1,36 +1,23 @@
 ﻿using Newtonsoft.Json;
 using PhillipsHueConsole;
+using PhillipsHueConsole.model;
 using System;
 
 namespace test.model {
     public class Light : HueComponent {
-        public override int ComponentKey { get; set; }
-        public string Name { get; set; }
-        public LightState State { get; set; }
-
-
-        public override string ToString() {
-            return $"{ComponentKey}: {Name} - {(State.On ? "ON" : "Off")}";
+        private LightState state;
+        public override string Key { get; set; }
+        public override string Name { get; set; }
+        public LightState State {
+            get { return state; }
+            set {
+                state = value;
+                state.Component = this;
+            }
         }
 
-        public struct LightState {
-            public bool On { get; set; }
-
-            [JsonProperty("bri")]
-            public int Brightness { get; set; }
-
-            public int Hue { get; set; }
-
-            [JsonProperty("sat")]
-            public int Saturation { get; set; }
-
-            public string Effect { get; set; }
-
-            public double[] XY { get; set; }
-
-            public override string ToString() {
-                return $"On: {On}\nBri: {Brightness}\nHue: {Hue}\nSat: {Saturation}\nEffect: {Effect}\nXY: [{XY[0]}, {XY[1]}]";
-            }
+        public override string ToString() {
+            return base.ToString() + $" - { (State.On ? "ON" : "Off")}";
         }
     }
 }
---
+++ PhillipsHueConsole/model/LightState.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test.model;
+
+namespace PhillipsHueConsole.model {
+    public class LightState {
+        public HueComponent Component { get; set; }
+
+        public bool On { get; set; }        
+        public int Bri { get; set; }        // Brightness  
+        public int Hue { get; set; }        
+        public int Sat { get; set; }        // Saturation
+        public string Effect { get; set; }  
+        public double[] XY { get; set; }
+
+        public override string ToString() {
+            return $"On: {On}\nBri: {Bri}\nHue: {Hue}\nSat: {Sat}\nEffect: {Effect}\nXY: [{XY[0]}, {XY[1]}]";
+        }
+    }
+
+}
